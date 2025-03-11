e35ad899c9f643ca0fab684f095c3ac609b21160
MARY ANN CERVANTES <maryanncervantes1228@gmail.com>
2025-03-11 22:41:02 +0800

    Create ODD_OR_EVEN.py

---
+++ ODD_OR_EVEN.py
@@ -0,0 +1,7 @@
+def checking_the_number():
+    num = int(input("Enter a number:"))
+    if num % 2 == 0:
+        print("Even number!")
+    else:
+        print("Odd number!")
+checking_the_number()
